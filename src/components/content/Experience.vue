db68e20837059546985cd33544e9683833e4deeb
Mark Ian <markiantalan@gmail.com>
2024-04-22 15:20:42 +0800

    add some additonal content

--- src/components/content/Experience.vue
+++ src/components/content/Experience.vue
@@ -1,37 +1,42 @@
 <template>
-  <div class="section-title" :class="elementIsAtTop ? 'active' : ''" v-if="mobile">
-    <div class="section-line"></div>
-    <p>career path.</p>
-  </div>
-  <div class="exp__container" id="experience" ref="sectionElement">
-    <content-header>
-      <template #title>career path.</template>
-    </content-header>
+ <div
+  class="section-title"
+  :class="elementIsAtTop ? 'active' : ''"
+  v-if="mobile"
+ >
+  <div class="section-line"></div>
+  <p>career path.</p>
+ </div>
+ <div class="exp__container" id="experience" ref="sectionElement">
+  <content-header>
+   <template #title>career path.</template>
+  </content-header>
 
-    <ul class="exp-content__wrapper" ref="elementToTrack">
-      <li class="exp-content-group" v-for="exp in experiences" :key="exp">
-        <div class="designation__wrapper">
-          <p class="position">{{ exp.position }}</p>
-          <p class="exp-date">{{ exp.date }}</p>
-        </div>
+  <ul class="exp-content__wrapper" ref="elementToTrack">
+   <li class="exp-content-group" v-for="exp in experiences" :key="exp">
+    <div class="designation__wrapper">
+     <p class="position">{{ exp.position }}</p>
+     <p class="exp-date">{{ exp.date }}</p>
+    </div>
 
-        <p class="company">
-          @{{ exp.company }}
-          <span v-if="!mobile"
-            >| <span>{{ exp.companyType }}</span></span
-          >
-        </p>
+    <p class="company">
+     @{{ exp.company }}
+     <span v-if="!mobile"
+      >| <span>{{ exp.companyType }}</span></span
+     >
+    </p>
 
-        <p class="desc">
-          {{ exp.desc }}
-        </p>
-      </li>
-    </ul>
+    <p class="desc">
+     {{ exp.desc }}
+    </p>
+   </li>
+  </ul>
 
-    <a href="/resume.pdf" target="_blank" class="resumeBtn"
-      >View Full Resume <span class="material-symbols-outlined"> arrow_forward </span></a
-    >
-  </div>
+  <a href="/resume.pdf" target="_blank" class="resumeBtn"
+   >View Full Resume
+   <span class="material-symbols-outlined"> arrow_forward </span></a
+  >
+ </div>
 </template>
 
 <script>
@@ -39,141 +44,146 @@ import { mapWritableState, mapActions } from "pinia";
 import useIndexStore from "@/store/index";
 
 export default {
-  name: "Experience",
-  data() {
-    return {
-      elementIsAtTop: false,
-      elAnimation: false,
-      experiences: [
-        {
-          position: "Jr. Web Developer",
-          date: "September 2022 - Present",
-          company: "APEC Homes",
-          companyType: "Real Estate Company",
-          desc:
-            "Worked on a team to redesign the internal Homeowners Online website using Figma. Developed the frontend using Vue.js and Tailwind CSS and integrated the existing API into the new UI. Managed the redesign of the internal After-Sales Integrated System. Developed a comprehensive design system, conducted manual testing, and collaborated with cross-functional teams to gather feedback and make necessary changes.",
-        },
-        {
-          position: "Graphic Designer",
-          date: "March 2020 - September 2022",
-          company: "APEC Homes",
-          companyType: "Real Estate Company",
-          desc:
-            "Collaborate as part of a team to design a high-fidelity web interface using Figma, contributing to the development of the new UI for an existing internal transmittal system. Created engaging graphics in collaboration with the Marketing and Social Media teams, which included unique logo designs. Designed captivating visual content for Facebook and PowerPoint presentations. Developed original Viber stickers to raise awareness about COVID-19. Collaborated on page layouts for the company handbook and consistently met project deadlines.",
-        },
-        {
-          position: "Graphic Designer",
-          date: "April 2019 - January 2020",
-          company: "ACC Hypermart Corp.",
-          companyType: "Retail Industry",
-          desc:
-            "Worked on a team to develop compelling designs for marketing packages, including print materials, banners, and signs. These designs effectively enhanced brand visibility and customer engagement. Collaborated closely with clients to ensure that design deliverables not only met but exceeded their expectations. Contributed to the achievement of sales quotas through effective teamwork and collaboration with peers.",
-        },
-      ],
-    };
-  },
-  computed: {
-    ...mapWritableState(useIndexStore, ["activeSection", "activeContent", "mobile"]),
-  },
-  mounted() {
-    window.addEventListener("scroll", this.checkIfElementAtTop);
-  },
-  beforeDestroy() {
-    window.removeEventListener("scroll", this.checkIfElementAtTop);
-  },
-  created() {
-    // Check whenever the screen size is changing
-    window.addEventListener("resize", this.checkScreen);
-    this.checkScreen();
-  },
-  methods: {
-    ...mapActions(useIndexStore, ["checkScreen"]),
-
-    checkIfElementAtTop() {
-      const element = this.$refs.elementToTrack;
-      const sectionBottom = this.$refs.sectionElement.getBoundingClientRect().bottom;
-
-      try {
-        if (element) {
-          const rect = element.getBoundingClientRect();
-          // Check if the element's top is at or above the top of the viewport
-          this.elementIsAtTop = rect.top <= 0;
-          // console.log(rect.top);
-
-          const expViewportHeight = window.innerHeight + 300;
-          const expSectionHalfwayPoint = rect.top + rect.height / 2;
-
-          // console.log(`section: ${expSectionHalfwayPoint}`);
-          // console.log(`viewport: ${expViewportHeight}`);
-
-          // trigger the animations
-          if (expSectionHalfwayPoint <= expViewportHeight) {
-          } else if (expSectionHalfwayPoint > expViewportHeight) {
-          }
-
-          if (this.elementIsAtTop) {
-            // Do something here
-          }
-          if (sectionBottom < 0) {
-            this.elementIsAtTop = false;
-          }
-        }
-      } catch (error) {
-        // console.log(error);
-      }
+ name: "Experience",
+ data() {
+  return {
+   elementIsAtTop: false,
+   elAnimation: false,
+   experiences: [
+    {
+     position: "Jr. Web Developer",
+     date: "September 2022 - Present",
+     company: "APEC Homes",
+     companyType: "Real Estate Company",
+     desc:
+      "Developed the internal MyAccess system from the ground up, including UI design, frontend implementation, and database integration via API, ensuring seamless user experiences and data handling. Worked collaboratively to redesign the internal Homeowners Online website using Figma, utilizing Vue.js and Tailwind CSS for frontend development and API integration. Managed the overhaul of the internal After-Sales Integrated System, including developing a comprehensive design system, conducting manual testing, and gathering feedback from cross-functional teams for necessary adjustments.",
     },
+    {
+     position: "Graphic Designer",
+     date: "March 2020 - September 2022",
+     company: "APEC Homes",
+     companyType: "Real Estate Company",
+     desc:
+      "Collaborate as part of a team to design a high-fidelity web interface using Figma, contributing to the development of the new UI for an existing internal transmittal system. Created engaging graphics in collaboration with the Marketing and Social Media teams, which included unique logo designs. Designed captivating visual content for Facebook and PowerPoint presentations. Developed original Viber stickers to raise awareness about COVID-19. Collaborated on page layouts for the company handbook and consistently met project deadlines.",
+    },
+    {
+     position: "Graphic Designer",
+     date: "April 2019 - January 2020",
+     company: "ACC Hypermart Corp.",
+     companyType: "Retail Industry",
+     desc:
+      "Worked on a team to develop compelling designs for marketing packages, including print materials, banners, and signs. These designs effectively enhanced brand visibility and customer engagement. Collaborated closely with clients to ensure that design deliverables not only met but exceeded their expectations. Contributed to the achievement of sales quotas through effective teamwork and collaboration with peers.",
+    },
+   ],
+  };
+ },
+ computed: {
+  ...mapWritableState(useIndexStore, [
+   "activeSection",
+   "activeContent",
+   "mobile",
+  ]),
+ },
+ mounted() {
+  window.addEventListener("scroll", this.checkIfElementAtTop);
+ },
+ beforeDestroy() {
+  window.removeEventListener("scroll", this.checkIfElementAtTop);
+ },
+ created() {
+  // Check whenever the screen size is changing
+  window.addEventListener("resize", this.checkScreen);
+  this.checkScreen();
+ },
+ methods: {
+  ...mapActions(useIndexStore, ["checkScreen"]),
+
+  checkIfElementAtTop() {
+   const element = this.$refs.elementToTrack;
+   const sectionBottom =
+    this.$refs.sectionElement.getBoundingClientRect().bottom;
+
+   try {
+    if (element) {
+     const rect = element.getBoundingClientRect();
+     // Check if the element's top is at or above the top of the viewport
+     this.elementIsAtTop = rect.top <= 0;
+     // console.log(rect.top);
+
+     const expViewportHeight = window.innerHeight + 300;
+     const expSectionHalfwayPoint = rect.top + rect.height / 2;
+
+     // console.log(`section: ${expSectionHalfwayPoint}`);
+     // console.log(`viewport: ${expViewportHeight}`);
+
+     // trigger the animations
+     if (expSectionHalfwayPoint <= expViewportHeight) {
+     } else if (expSectionHalfwayPoint > expViewportHeight) {
+     }
+
+     if (this.elementIsAtTop) {
+      // Do something here
+     }
+     if (sectionBottom < 0) {
+      this.elementIsAtTop = false;
+     }
+    }
+   } catch (error) {
+    // console.log(error);
+   }
   },
+ },
 };
 </script>
 
 <style lang="postcss" scoped>
 .exp__container {
-  @apply h-full min-h-screen;
-  /*  py-[6rem] */
+ @apply h-full min-h-screen;
+ /*  py-[6rem] */
 }
 
 .exp-content__wrapper {
-  @apply grid gap-6;
+ @apply grid gap-6;
 }
 
 .position,
 .exp-date,
 .company {
-  @apply text-neutral-800;
+ @apply text-neutral-800;
 }
 
 .designation__wrapper {
-  @apply py-[.625rem];
+ @apply py-[.625rem];
 }
 
 .position {
-  @apply text-xl font-semibold;
+ @apply text-xl font-semibold;
 }
 
 .exp-date {
-  @apply text-xs font-code font-normal text-neutral-500;
+ @apply text-xs font-code font-normal text-neutral-500;
 }
 
 .company {
-  @apply text-sm font-medium bg-neutral-100 py-[.3rem] px-[.6rem] rounded;
+ @apply text-sm font-medium bg-neutral-100 py-[.3rem] px-[.6rem] rounded;
 }
 
 .desc {
-  @apply text-sm text-neutral-500 pl-[1.8rem] my-[.9rem] border-l-[1px] border-primary-500;
+ @apply text-sm text-neutral-500 pl-[1.8rem] my-[.9rem] border-l-[1px] border-primary-500;
 }
 
 .resumeBtn {
-  @apply flex items-center gap-2 font-code text-sm font-semibold text-neutral-800 mt-[3rem] hover:gap-6;
-  transition: 0.3s;
+ @apply flex items-center gap-2 font-code text-sm font-semibold text-neutral-800 mt-[3rem] hover:gap-6;
+ transition: 0.3s;
 }
 
 .resumeBtn span {
-  @apply text-base font-semibold;
+ @apply text-base font-semibold;
 }
 
 @media (min-width: 600px) {
-  .designation__wrapper {
-    @apply flex items-center justify-between;
-  }
+ .designation__wrapper {
+  @apply flex items-center justify-between;
+ }
 }
 </style>
--- src/components/content/Skills.vue
+++ src/components/content/Skills.vue
@@ -1,111 +1,115 @@
 <template>
-  <div class="skills__container">
-    <p class="gen-contentText">
-      Here you will find more information about me, what I do, and my current skills
-      mostly in terms of programming and technology.
+ <div class="skills__container">
+  <p class="gen-contentText">
+   Here you will find more information about me, what I do, and my current
+   skills mostly in terms of programming and technology.
+  </p>
+
+  <ul class="skills__wrapper">
+   <li class="skills-group" v-for="skill in skills" :key="skill.title">
+    <p class="title">{{ skill.title }}</p>
+    <p class="description">
+     {{ skill.description }}
     </p>
 
-    <ul class="skills__wrapper">
-      <li class="skills-group" v-for="skill in skills" :key="skill.title">
-        <p class="title">{{ skill.title }}</p>
-        <p class="description">
-          {{ skill.description }}
-        </p>
-
-        <ul class="skills-lists">
-          <li v-for="tag in skill.skills" :key="tag">{{ tag }}</li>
-        </ul>
-      </li>
+    <ul class="skills-lists">
+     <li v-for="tag in skill.skills" :key="tag">{{ tag }}</li>
     </ul>
-  </div>
+   </li>
+  </ul>
+ </div>
 </template>
 
 <script>
 export default {
-  name: "Skills",
-  data() {
-    return {
-      skills: [
-        {
-          title: "Designer",
-          description:
-            "I value minimalist content, polished design, and deliberate interactions.",
-          skills: [
-            "UI/UX",
-            "Wireframing",
-            "Prototyping",
-            "Mockups",
-            "Design System",
-            "Posters",
-          ],
-        },
-        {
-          title: "Developer",
-          description:
-            "I relish coding and transforming ideas into vibrant web experiences.",
-          skills: [
-            "HTML5",
-            "CSS",
-            "Sass",
-            "Git",
-            "Tailwind CSS",
-            "Firebase",
-            "PHP",
-            "Javascript (ES6+)",
-            "RESTful API",
-            "Vue.js",
-            "Responsive Design",
-            "MySQL",
-          ],
-        },
-        {
-          title: "Toolkit",
-          description: "My daily tools for expertise elevation and exceptional results.",
-          skills: [
-            "VS Code",
-            "Figma",
-            "Gitbash",
-            "Postman",
-            "Flowchart",
-            "Notion",
-            "Github",
-            "Adobe Illustrator",
-            "Adobe Photoshop",
-            "StackBlitz",
-            "XAMPP-phpMyAdmin",
-            "Blender",
-          ],
-        },
-      ],
-    };
-  },
+ name: "Skills",
+ data() {
+  return {
+   skills: [
+    {
+     title: "Designer",
+     description:
+      "I value minimalist content, polished design, and deliberate interactions.",
+     skills: [
+      "UI/UX",
+      "Wireframing",
+      "Prototyping",
+      "Mockups",
+      "Design System",
+      "Posters",
+     ],
+    },
+    {
+     title: "Developer",
+     description:
+      "I relish coding and transforming ideas into vibrant web experiences.",
+     skills: [
+      "HTML5",
+      "CSS",
+      "Sass",
+      "Git",
+      "Tailwind CSS",
+      "Firebase",
+      "PHP",
+      "Laravel",
+      "Javascript (ES6+)",
+      "RESTful API",
+      "Vue.js",
+      "Responsive Design",
+      "MySQL",
+      "Netlify",
+     ],
+    },
+    {
+     title: "Toolkit",
+     description:
+      "My daily tools for expertise elevation and exceptional results.",
+     skills: [
+      "VS Code",
+      "Figma",
+      "Gitbash",
+      "Postman",
+      "Flowchart",
+      "Notion",
+      "Github",
+      "Adobe Illustrator",
+      "Adobe Photoshop",
+      "StackBlitz",
+      "XAMPP-phpMyAdmin",
+      "Blender",
+      "MySQL Workbench",
+     ],
+    },
+   ],
+  };
+ },
 };
 </script>
 
 <style lang="postcss" scoped>
 .skills__container {
-  @apply mt-[5rem];
+ @apply mt-[5rem];
 }
 
 .skills__wrapper {
-  @apply grid gap-12 mt-[2.5rem];
+ @apply grid gap-12 mt-[2.5rem];
 }
 
 .skills-group .title {
-  @apply text-xl text-neutral-800 font-code font-semibold mb-[.6rem];
+ @apply text-xl text-neutral-800 font-code font-semibold mb-[.6rem];
 }
 
 .skills-group .description {
-  @apply text-sm text-neutral-600;
+ @apply text-sm text-neutral-600;
 }
 
 .skills-lists {
-  @apply flex flex-wrap gap-3 mt-[1.2rem] max-w-[95%];
+ @apply flex flex-wrap gap-3 mt-[1.2rem] max-w-[95%];
 }
 
 .skills-lists li {
-  @apply relative text-xs font-code bg-neutral-200 min-w-fit max-w-fit
+ @apply relative text-xs font-code bg-neutral-200 min-w-fit max-w-fit
     px-[1rem] py-[.6rem] rounded-full outline outline-[.01rem] outline-neutral-300;
-  /* box-shadow: 0px 1px 2px -1px rgba(0, 0, 0, 0.1), 0px 1px 3px 0px rgba(0, 0, 0, 0.1); */
+ /* box-shadow: 0px 1px 2px -1px rgba(0, 0, 0, 0.1), 0px 1px 3px 0px rgba(0, 0, 0, 0.1); */
 }
 </style>
